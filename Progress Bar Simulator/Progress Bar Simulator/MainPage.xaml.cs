86ec3c70527b66deb5e114bd5f52fc932b276b6c
Marv Etuk <Marvin.Etuk@gmail.com>
2021-01-04 15:11:48 +0000

    Slider colours now update to show MainProgressBar's ProgressColour.
    Shortened ActivateProgressBarAsync. Debating whether the method used is better or not.
    
    Made variable and method names clearer and added comments.

--- Progress Bar Simulator/Progress Bar Simulator/MainPage.xaml.cs
+++ Progress Bar Simulator/Progress Bar Simulator/MainPage.xaml.cs
@@ -10,106 +10,111 @@ namespace Progress_Bar_Simulator
 {
     public partial class MainPage : ContentPage
     {
-        int MainProgressBarLoadCount = 0;
-        private bool _MainButtonActivated = false;
-        bool MainButtonActivated
+        private int MainProgressBarLoadCount = 0;
+        private bool _IsMainProgressBarActivated = false;
+        private bool IsMainProgressBarActivated
         {
             get
             {
-                return _MainButtonActivated;
+                return _IsMainProgressBarActivated;
             }
             set
             {
-                _MainButtonActivated = value;
-                MainButton.IsEnabled = !value;
-                MainButton.BackgroundColor = value ? Color.FromHex("#35D435") : Color.FromHex("#D43535");
-                MainButton.Text = value ? "In Progress" : "Not Activated";
+                _IsMainProgressBarActivated = value;
+                BtnLoadMainProgressBar.IsEnabled = !value;
+                BtnLoadMainProgressBar.BackgroundColor = value ? Color.FromHex("#35D435") : Color.FromHex("#D43535");
+                BtnLoadMainProgressBar.Text = value ? "In Progress" : "Not Activated";
             }
         }
 
         public MainPage()
         {
             InitializeComponent();
+            //Set the values of the sliders to the initial colour of MainProgressBar's progress colour.
             SliRed.Value = 250;
             SliGreen.Value = 82;
             SliBlue.Value = 82;
+
         }
-        private async void MainButton_Clicked(object sender, EventArgs e)
+
+        #region "Methods"
+        /// <summary>
+        /// Animate the MainProgressBar based on the radiobutton selected.
+        /// </summary>
+        private async Task AnimateMainProgressBarAsync()
         {
-            await ActivateProgressBarAsync();
+            uint ProgressBarAnimationLength = 3000;
+            Easing MainProgressBarEasing = Easing.CubicInOut;
+
+            Dictionary<String, Easing> EasingChoices = new Dictionary<string, Easing>()
+            {
+                {"Cubic In", Easing.CubicIn},
+                {"Cubic Out", Easing.CubicOut},
+                {"Cubic In Out", Easing.CubicInOut},
+                {"Sin In", Easing.SinIn},
+                {"Sin Out", Easing.SinOut},
+                {"Sin In Out", Easing.SinInOut},
+                {"Bounce In", Easing.BounceIn},
+                {"Bounce Out", Easing.BounceOut},
+                {"Spring In", Easing.SpringIn},
+                {"Spring Out", Easing.SpringOut},
+                {"Linear", Easing.Linear}
+            };
+
+            //Update the easing mode according to whichever radiobutton is checked.
+            foreach (RadioButton RadLoadOption in GrdLoadModeOptions.Children.OfType<RadioButton>().Where(x => x.IsChecked))
+            {
+                try
+                {
+                    MainProgressBarEasing = EasingChoices[RadLoadOption.Text];
+                }
+                catch
+                {
+                    await DisplayAlert("Error", "A non-existant load mode was selected", "Cancel");
+                }
+            }
+            //Start the MainProgressBar animation and update the load count after it's done.
+            await MainProgressBar.ProgressTo(1, ProgressBarAnimationLength, MainProgressBarEasing);
+            await Task.Delay(250);
+            MainProgressBar.Progress = 0;
+            MainProgressBarLoadCount += 1;
+            IsMainProgressBarActivated = false;
+            LblLoadCount.FormattedText.Spans[1].Text = MainProgressBarLoadCount.ToString();
+        }
+        /// <summary>
+        /// Update MainProgressBar's progress colour and update the colour of the sliders to match it.
+        /// </summary>
+        private void UpdateMainProgressBarColour()
+        {
+            MainProgressBar.ProgressColor = new Color(SliRed.Value / 255, SliGreen.Value / 255, SliBlue.Value / 255);
+            SliRed.MaximumTrackColor = MainProgressBar.ProgressColor;
+            SliGreen.MaximumTrackColor = MainProgressBar.ProgressColor;
+            SliBlue.MaximumTrackColor = MainProgressBar.ProgressColor;
+        }
+        #endregion
+
+        #region "Event Handlers"
+        private async void BtnLoadMainProgressBar_Clicked(object sender, EventArgs e)
+        {
+            IsMainProgressBarActivated = !IsMainProgressBarActivated;
+            if (IsMainProgressBarActivated)
+            {
+                await AnimateMainProgressBarAsync();
+            }
         }
         private void SliRed_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            MainProgressBar.ProgressColor = new Color(SliRed.Value / 255, SliGreen.Value / 255, SliBlue.Value / 255);
+            UpdateMainProgressBarColour();
         }
         private void SliGreen_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            MainProgressBar.ProgressColor = new Color(SliRed.Value / 255, SliGreen.Value / 255, SliBlue.Value / 255);
+            UpdateMainProgressBarColour();
         }
         private void SliBlue_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            MainProgressBar.ProgressColor = new Color(SliRed.Value / 255, SliGreen.Value / 255, SliBlue.Value / 255);
+            UpdateMainProgressBarColour();
         }
-        private async Task ActivateProgressBarAsync()
-        {
-            uint ProgressLength = 3000;
-            MainButtonActivated = !MainButtonActivated;
-            if (MainButtonActivated)
-            {
-
-                if (RadBounceIn.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.BounceIn);
-                }
-                else if (RadBounceOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.BounceOut);
-                }
-                else if (RadCubicIn.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.CubicIn);
-                }
-                else if (RadCubicOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.CubicOut);
-                }
-                else if (RadCubicInOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.CubicInOut);
-                }
-                else if (RadLinear.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.Linear);
-                }
-                else if (RadSinIn.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.SinIn);
-                }
-                else if (RadSinOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.SinOut);
-                }
-                else if (RadSinInOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.SinInOut);
-                }
-                else if (RadSpringIn.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.SpringIn);
-                }
-                else if (RadSpringOut.IsChecked)
-                {
-                    await MainProgressBar.ProgressTo(1, ProgressLength, Easing.SpringOut);
-                }
-                await Task.Delay(250);
-                MainProgressBar.Progress = 0;
-                MainProgressBarLoadCount += 1;
-                MainButtonActivated = false;
-                LblLoadCount.FormattedText.Spans[1].Text = MainProgressBarLoadCount.ToString();
-            }
-        }
-
-
-
+        #endregion
     }
 }
+
